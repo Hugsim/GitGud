c00badf63e470e2474d4ec19829d935d9c9f368f
Robert Sjöberg <98rosj19@elev.kungalv.se>
2017-05-17 13:12:46 +0200

    fixat login från logout

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php 
 function logout(){
     echo "Du har loggat ut!<br>";
-    echo '<a href="auth.html">Logga in igen!</a>';
+    echo '<a href="login.html">Logga in igen!</a>';
     header('HTTP/1.1 401 Unauthorized'); //själva "utloggningen", alltså omdirigerad till en 401 sida
     return true;
 }
