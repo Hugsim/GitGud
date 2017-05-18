253bbf3c8cb85dc3beaf985eee5aac8e156341c5
Hugo Simonsson <99husi30@elev.kungalv.se>
2017-05-18 15:56:15 +0200

    Utloggning bättre kanske?

--- logout.php
+++ logout.php
@@ -9,9 +9,12 @@
 
 <?php 
 function logout(){
+    session_start();
     echo "Du har loggat ut!<br>";
     echo '<a href="login.html">Logga in igen!</a>';
     header('HTTP/1.1 401 Unauthorized'); //själva "utloggningen", alltså omdirigerad till en 401 sida
+    session_unset();
+    session_destroy();
     return true;
 }
 
